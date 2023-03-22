7405140de56207f4eaa126b8beba4db0aa75e58a
Andy Short <automation.andy101@gmail.com>
2023-03-22 21:40:10 +0700

    Added project component

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -14,19 +14,19 @@ export const ProjectList = [
     {
         name: 'Python Selenium BDD Automated UI Testing Framework',
         image: PythonSelenium,
-        skills: 'Javascript, Playwright, Cucumber BDD, HTML Reporting',
+        skills: 'Python, Selenium, Cucumber BDD',
         github: 'https://github.com/automation-andy101/playwright-ui-bdd-reporting-demo'
     },
     {
         name: 'SpecFlow and Selenium BDD Automated UI Testing Framework with Reporting',
         image: SpecFlow,
-        skills: 'Javascript, Playwright, Cucumber BDD, HTML Reporting',
+        skills: 'C#, SpecFlow, Selenium, Cucumber BDD, HTML Reporting',
         github: 'https://github.com/automation-andy101/playwright-ui-bdd-reporting-demo'
     },
     {
         name: 'Pact API Contract Testing Framework',
         image: Pact,
-        skills: 'Javascript, Playwright, Cucumber BDD, HTML Reporting',
+        skills: 'Pact, PactFlow, Javascript',
         github: 'https://github.com/automation-andy101/playwright-ui-bdd-reporting-demo'
     },
 ]
