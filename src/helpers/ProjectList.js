e601be79b1f9f4c3fcf2a08f6f7a1abc289bd2c7
Andy Short <automation.andy101@gmail.com>
2023-03-22 19:13:12 +0700

    Started adding projects component

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,19 @@
+import Playwright from '../assets/playwright_00001.png';
+import PythonSelenium from '../assets/python-selenium.png';
+import Pact from '../assets/pact.png';
+
+
+export const ProjectList = [
+    {
+        name: 'Playwright BDD Automated UI Testing Framework with HTML Reporting',
+        image: Playwright
+    },
+    {
+        name: 'Python Selenium BDD Automated UI Testing Framework',
+        image: PythonSelenium
+    },
+    {
+        name: 'Pact API Contract Testing Framework',
+        image: Pact
+    },
+]
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import ProjectItem from '../components/ProjectItem';
+import { ProjectList } from '../helpers/ProjectList';
+import '../styles/Projects.css';
+
 
 const Projects = () => {
   return (
-    <div>
-      
+    <div className='projects'>
+      <h1>My Personal Projects</h1>
+      <div className='projectList'>
+        { ProjectList.map((projectItem) => {
+          return <ProjectItem name={projectItem.name} image={projectItem.image} />
+        })}
+      </div>
     </div>
   )
 }
