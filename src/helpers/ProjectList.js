8af630e5c741fdba80375e6ffb7c53031b89d806
Andy Short <automation.andy101@gmail.com>
2023-03-28 13:34:53 +0700

    Added all my personal test framework projects

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -3,6 +3,10 @@ import PythonSelenium from '../assets/python-selenium.png';
 import Pact from '../assets/pact-2.png';
 import SpecFlow from '../assets/specflow.png';
 import Appium from '../assets/appium.jpg';
+import PythonRequests from '../assets/python-requests.jpg';
+import CypressUI from '../assets/cypress-ui.png';
+import CypressAPI from '../assets/cypress-api.png';
+import RestAssured from '../assets/restassured.png';
 
 
 export const ProjectList = [
@@ -13,27 +17,52 @@ export const ProjectList = [
         github: 'https://github.com/automation-andy101/playwright-ui-bdd-reporting-demo'
     },
     {
-        name: 'Python Selenium BDD Automated UI Testing Framework',
-        image: PythonSelenium,
-        skills: 'Python, Selenium, Cucumber BDD',
-        github: 'https://github.com/automation-andy101/playwright-ui-bdd-reporting-demo'
-    },
-    {
         name: 'SpecFlow and Selenium BDD Automated UI Testing Framework with Reporting',
         image: SpecFlow,
         skills: 'C#, SpecFlow, Selenium, Cucumber BDD, HTML Reporting',
-        github: 'https://github.com/automation-andy101/playwright-ui-bdd-reporting-demo'
+        github: 'https://github.com/automation-andy101/csharp-selenium-specflow-framework'
+    },
+    {
+        name: 'Python Selenium BDD Automated UI Testing Framework',
+        image: PythonSelenium,
+        skills: 'Python, Selenium, Cucumber BDD',
+        github: 'https://github.com/automation-andy101/python-selenium-bdd-demo'
+    },
+    {
+        name: 'Cypress BDD Automated UI Testing Framework with HTML Reporting',
+        image: CypressUI,
+        skills: 'Javascript, Cypress, Cucumber BDD, HTML Reporting',
+        github: 'https://github.com/automation-andy101/cypress-v10-ui-bdd-test-framework'
+    },
+    {
+        name: 'Python Requests Library BDD API Testing Framework Testing',
+        image: PythonRequests,
+        skills: 'Python, Requests library, Cucumber BDD',
+        github: 'https://github.com/automation-andy101/python-requests-api-bdd-demo'
+    },
+    {
+        name: 'RestAssured API Testing Framework Testing with Appium Testing Framework',
+        image: RestAssured,
+        skills: 'Jave, RestAssured, Cucumber BDD, POJO classes',
+        github: 'https://github.com/automation-andy101/restassured-api-bdd-test-framework'
+    },
+    {
+        name: 'Cypress 10 Cucumber BDD API Testing Framework with HTML Reporting',
+        image: CypressAPI,
+        skills: 'Cypress, Javascript, Cucumber BDD, HTML reporting',
+        github: 'https://github.com/automation-andy101/cypress-api-bdd-test-framework'
     },
     {
         name: 'Pact API Contract Testing Framework',
         image: Pact,
         skills: 'Pact, PactFlow, Javascript',
-        github: 'https://github.com/automation-andy101/playwright-ui-bdd-reporting-demo'
+        github: 'https://github.com/automation-andy101/pactjs-api-contract-test-framework'
     },
     {
         name: 'Mobile Testing with Appium Testing Framework',
         image: Appium,
         skills: 'Jave, Appium, TestNG',
-        github: 'https://github.com/divine-moment-of-truth/appium-automated-test-framework'
-    },
+        github: 'https://github.com/automation-andy101/appium-mobile-test-framework'
+    },    
+
 ]
